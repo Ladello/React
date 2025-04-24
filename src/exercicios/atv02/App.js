004721f086f00f814d01eebcd6527c32b494c77e
Gustavo Assis Coladello <gustavo.coladello@gmail.com>
2025-04-23 22:48:22 -0300

    Bug fix

---
+++ src/exercicios/atv02/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Contador from './Contador';
+import './App.css';
+
+export default function Atv02() {
+  const [homens, setHomens] = useState(0);
+  const [mulheres, setMulheres] = useState(0);
+
+  const incrementarHomens = () => setHomens(homens + 1);
+  const decrementarHomens = () => setHomens(homens - 1);
+
+  const incrementarMulheres = () => setMulheres(mulheres + 1);
+  const decrementarMulheres = () => setMulheres(mulheres - 1);
+
+  return (
+    <div className="atv02-container">
+      <div className="contador-box">
+        <Contador
+          nome="Homens"
+          imgSrc="https://cdn4.iconfinder.com/data/icons/eldorado-navigation/40/human-512.png"
+          valor={homens}
+          onIncrement={incrementarHomens}
+          onDecrement={decrementarHomens}
+        />
+        <Contador
+          nome="Mulheres"
+          imgSrc="https://cdn4.iconfinder.com/data/icons/eldorado-navigation/40/woman-512.png"
+          valor={mulheres}
+          onIncrement={incrementarMulheres}
+          onDecrement={decrementarMulheres}
+        />
+      </div>
+      <div className="total-container">
+        <h3>Total: {homens + mulheres}</h3>
+      </div>
+    </div>
+  );
+}
--- src/exercicios/atv02/index.js
+++ src/exercicios/atv02/index.js
@@ -1,39 +1,11 @@
-import React, { useState } from 'react';
-import Contador from './Contador';
-import './index.css'; // Alterei para usar index.css que é o seu estilo padrão
+import React from 'react';
+import App from './App';
 
 export default function Atv02() {
-  const [homens, setHomens] = useState(0);
-  const [mulheres, setMulheres] = useState(0);
-
-  const incrementarHomens = () => setHomens(homens + 1);
-  const decrementarHomens = () => setHomens(homens - 1);
-
-  const incrementarMulheres = () => setMulheres(mulheres + 1);
-  const decrementarMulheres = () => setMulheres(mulheres - 1);
-
   return (
-    <div className="atv02-container">
+    <div>
       <h2>Atividade 02</h2>
-      <div className="contador-box">
-        <Contador
-          nome="Homens"
-          imgSrc="https://cdn4.iconfinder.com/data/icons/eldorado-navigation/40/human-512.png"
-          valor={homens}
-          onIncrement={incrementarHomens}
-          onDecrement={decrementarHomens}
-        />
-        <Contador
-          nome="Mulheres"
-          imgSrc="https://cdn4.iconfinder.com/data/icons/eldorado-navigation/40/woman-512.png"
-          valor={mulheres}
-          onIncrement={incrementarMulheres}
-          onDecrement={decrementarMulheres}
-        />
-      </div>
-      <div className="total-container">
-        <h3>Total: {homens + mulheres}</h3>
-      </div>
+      <App />
     </div>
   );
 }
