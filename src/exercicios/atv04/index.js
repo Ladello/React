1c5803ba91b8939a51fc75de3af7cb1348cc7af3
Ladello <gustavo.coladello@gmail.com>
2025-05-27 11:27:32 -0300

    Bug fix atv04

--- src/exercicios/atv04/index.js
+++ src/exercicios/atv04/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import {Link} from "react-router-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-  <h1>Atividade 04</h1>
-  <App />
-  <Link to="/">Acesse a home</Link>
-  
-  </>
-);
-
+export default function Atv04() {
+  return (
+    <div>
+      <h2>Atividade 04</h2>
+      <App />
+      <br />
+      <Link to="/">Acesse a home</Link>
+    </div>
+  );
+}
